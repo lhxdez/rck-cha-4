99ba840b48efb38d3e9a57c86530fe12fc7439df
Lucas da Silveira Gomes <94697673+LucasGomes98@users.noreply.github.com>
2022-06-15 10:10:53 -0300

    feat: add test answers

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,18 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const isAdminUser = this.usersRepository.findById(user_id);
+
+    if (isAdminUser.admin === false) {
+      throw new Error("Not an admin user");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
